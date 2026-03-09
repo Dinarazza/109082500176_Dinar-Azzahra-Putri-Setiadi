3b4bdaae8f166db198149da487cc8a9f49549442
Dinarazza <Dinarazza28@gmail.com>
2026-03-09 16:06:31 +0700

    tes commit

--- Week 2 - Modul 2/Guided/Guided- 2/targetTabungan.go
+++ Week 2 - Modul 2/Guided/Guided- 2/targetTabungan.go
@@ -18,7 +18,6 @@ func main() {
         total = total + tabungan
     }
 
-    // Output setelah loop selesai (target tercapai)
     fmt.Printf("Selamat! Target tercapai dalam %d hari.\n", hari)
     fmt.Printf("Total tabungan Anda terkumpul: Rp%d\n", total)
 }

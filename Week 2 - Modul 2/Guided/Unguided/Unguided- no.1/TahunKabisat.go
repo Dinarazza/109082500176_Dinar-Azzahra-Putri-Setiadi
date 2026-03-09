37f1cc3be48806844459540f243d095321098811
Dinarazza <Dinarazza28@gmail.com>
2026-03-09 19:57:34 +0700

    tes commit

---
+++ Week 2 - Modul 2/Guided/Unguided/Unguided- no.1/TahunKabisat.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isKabisat(tahun int) bool {
+	if tahun%400 == 0 {
+		return true
+	} else if tahun%4 == 0 && tahun%100 != 0 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	var tahun int
+
+	fmt.Print("Masukkan tahun : ")
+	fmt.Scan(&tahun)
+
+	hasil := isKabisat(tahun)
+
+	fmt.Println("Kabisat :", hasil)
+}

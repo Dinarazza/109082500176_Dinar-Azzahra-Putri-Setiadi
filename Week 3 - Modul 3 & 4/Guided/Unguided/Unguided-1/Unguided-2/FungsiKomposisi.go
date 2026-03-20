ac4bcf871d60ba6676641c107e17a2e5da066c9e
Dinarazza <Dinarazza28@gmail.com>
2026-03-21 01:44:20 +0700

    tess commit

---
+++ Week 3 - Modul 3 & 4/Guided/Unguided/Unguided-1/Unguided-2/FungsiKomposisi.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func f(x int) int {
+	return x * x
+}
+
+func g(x int) int {
+	return x - 2
+}
+
+func h(x int) int {
+	return x + 1
+}
+
+func main() {
+	var a, b, c int
+
+	fmt.Print("Masukkan nilai a : ")
+	fmt.Scan(&a)
+	fmt.Print("Masukkan nilai b : ")
+	fmt.Scan(&b)
+	fmt.Print("Masukkan nilai c : ")
+	fmt.Scan(&c)
+
+	fmt.Printf("F(G(H( %d ))) : %d\n", a, f(g(h(a))))
+
+	fmt.Printf("G(H(F( %d ))) : %d\n", b, g(h(f(b))))
+
+	fmt.Printf("H(F(G( %d ))) : %d\n", c, h(f(g(c))))
+}

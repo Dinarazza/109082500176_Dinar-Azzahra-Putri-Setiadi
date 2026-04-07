3aa1650f029fc0f2e12ff3ef585adfd007af3f60
Dinarazza <Dinarazza28@gmail.com>
2026-04-07 15:57:29 +0700

    tes commit

---
+++ Asesmen 1/timbangan.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+const pi float64 = 3.14
+
+func volume(r, t float64) float64 {
+	return pi * r * r * t
+}
+
+func massa(r, t, p float64) float64 {
+	return volume(r, t) * p
+}
+
+func display(m1, m2 float64) {
+	if m1 == m2 {
+		fmt.Println("BALANCE")
+	} else {
+		selisih := m1 - m2
+		if selisih < 0 {
+			selisih = -selisih
+		}
+		fmt.Println("Selisih massa:", selisih)
+	}
+}
+
+func main() {
+	var r float64
+	var tKiri, tKanan float64
+	var mJenisKiri, mJenisKanan float64
+	var massaKiri, massaKanan float64
+
+	fmt.Print("Masukkan jari-jari tabung: ")
+	fmt.Scan(&r)
+
+	fmt.Print("Masukkan tinggi zat cair kiri: ")
+	fmt.Scan(&tKiri)
+	fmt.Print("Masukkan massa jenis kiri: ")
+	fmt.Scan(&mJenisKiri)
+
+	fmt.Print("Masukkan tinggi zat cair kanan: ")
+	fmt.Scan(&tKanan)
+	fmt.Print("Masukkan massa jenis kanan: ")
+	fmt.Scan(&mJenisKanan)
+
+	massaKiri = massa(r, tKiri, mJenisKiri)
+	massaKanan = massa(r, tKanan, mJenisKanan)
+
+	display(massaKiri, massaKanan)
+}

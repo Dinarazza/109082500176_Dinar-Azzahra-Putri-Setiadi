1407307efa8e643b5beef88ca932e21d54200633
Dinarazza <Dinarazza28@gmail.com>
2026-04-20 19:13:48 +0700

    tes commit

---
+++ Week 7 - Modul 7/Guided-1/desimal.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type desimal float64
+
+func main() {
+	var alas, tinggi, luas desimal
+
+	fmt.Print("Masukan Pnajang Alas Segitiga: ")
+	fmt.Scan(&alas)
+
+	fmt.Print("Masukan Tinggi Segitiga: ")
+	fmt.Scan(&tinggi)
+
+	luas = 0.5 * alas * tinggi
+
+	fmt.Printf("Luas Segitiga dengan alas %.2f dan tinggi %.2f adalah %.2f\n", alas, tinggi, luas)
+}

adc4fcf934a636f1dc38becbe8ed6c438cc6e7bb
Dinarazza <Dinarazza28@gmail.com>
2026-04-27 19:54:18 +0700

    tes commit

---
+++ Week 9 - Modul 9/Unguided-1/lingkaran.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Titik struct {
+	x, y int
+}
+
+type Lingkaran struct {
+	pusat Titik
+	r     int
+}
+
+func jarak(p, q Titik) float64 {
+	return math.Sqrt(math.Pow(float64(p.x-q.x), 2) + math.Pow(float64(p.y-q.y), 2))
+}
+
+func didalam(c Lingkaran, p Titik) bool {
+	return jarak(c.pusat, p) <= float64(c.r)
+}
+
+func main() {
+	var l1, l2 Lingkaran
+	var p Titik
+
+	fmt.Scan(&l1.pusat.x, &l1.pusat.y, &l1.r)
+	fmt.Scan(&l2.pusat.x, &l2.pusat.y, &l2.r)
+	fmt.Scan(&p.x, &p.y)
+
+	dalam1 := didalam(l1, p)
+	dalam2 := didalam(l2, p)
+
+	if dalam1 && dalam2 {
+		fmt.Println("Titik di dalam lingkaran 1 dan 2")
+	} else if dalam1 {
+		fmt.Println("Titik di dalam lingkaran 1")
+	} else if dalam2 {
+		fmt.Println("Titik di dalam lingkaran 2")
+	} else {
+		fmt.Println("Titik di luar lingkaran 1 dan 2")
+	}
+}

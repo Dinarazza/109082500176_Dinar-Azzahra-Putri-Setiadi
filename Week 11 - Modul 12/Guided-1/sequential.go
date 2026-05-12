3499ecf478bd650ce628cf58da41a1682e5c46db
Dinarazza <Dinarazza28@gmail.com>
2026-05-12 16:05:19 +0700

    tes commit

---
+++ Week 11 - Modul 12/Guided-1/sequential.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type arrData [5]string
+
+func seqSearch(arr arrData, namaBintang string) int {
+	var found bool = false
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == Bintangcari {
+			found = i
+		}
+	}
+	return found
+}
+
+func main() {
+	var arrBintang arrData
+
+	for i := 0; i < len(arrBintang); i++ {
+		fmt.Printf("Masukan data bintang ke-%d:", i)
+		fmt.Scan(&arrBintang[i])
+	}
+	fmt.Println()
+
+	var bintangCari string
+	fmt.Print("Masukan nama bintang yang mau dicari :")
+	fmt.Scan(&bintangCari)
+
+	var idxCari int
+	idxCari = seqSearch(arrBintang, bintangCari)
+
+	if idxCari > -1 {
+		fmt.Printf("Data %s ditemukan pada indeks ke-%d!", bintangCari, idxCari)
+	} else if idxCari == -1 {
+		fmt.Printf("Data %s tidak ditemukan!", bintangCari)
+	}
+}
